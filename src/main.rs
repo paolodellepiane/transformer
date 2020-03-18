6280e8dd554a9b6bda90f848f7b383502bef0542
pd <pd29@gmail.com>
2020-03-18 17:55:58 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-use std::{path::Path, fs::File, io::{Read, BufReader}, error::Error};
-use serde_json::{Value};
+use serde_json::Value;
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufReader, Read},
+    path::Path,
+};
 
 fn main() {
-    let json: Value = read_json_from_file("C:\\Users\\pdell\\w\\rs\\transformer\\appsettings.json").expect("JSON was not well-formatted");    
-    println!("{:?}", json.as_object().unwrap().len());
+    let json: Value = read_json_from_file("appsettings.json").expect("JSON was not well-formatted");
+    println!("{}", json.as_object().unwrap().len());
 }
 
 fn read_json_from_file<P: AsRef<Path>>(path: P) -> Result<Value, Box<dyn Error>> {
-    let pippo = "{
-    \"I\": false,
-}";
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let u: Value = serde_json::from_reader(reader)?;
+    Ok(u)
+}
 
+fn read_json_from_file2<P: AsRef<Path>>(path: P) -> Result<Value, Box<dyn Error>> {
     let mut file = File::open(path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let reader = BufReader::new(file);
     let u: Value = serde_json::from_str(&contents)?;
     Ok(u)
 }
