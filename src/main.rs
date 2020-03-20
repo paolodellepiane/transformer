feee36d4abd1b9111c79582006dbcfa2c23bf0db
pd <pd29@gmail.com>
2020-03-20 17:07:41 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
 use serde_json::Value;
+use std::io::Seek;
 use std::{
     error::Error,
     fs::File,
-    io::{BufReader, Read},
+    io::{BufReader, Read, SeekFrom},
     path::Path,
 };
 
 fn main() {
-    let json: Value = read_json_from_file("appsettings.json").expect("JSON was not well-formatted");
-    println!("{}", json.as_object().unwrap().len());
+    let v = read_json_from_file("appsettings-bom.json").expect("error parsing JSON");
+    let prefix = "";
+    transform(&v, prefix).expect("error transforming");
+}
+
+fn transform(value: &Value, prefix: &str) -> Result<(), Box<dyn Error>> {
+    for (n, v) in value.as_object().unwrap().iter() {
+        let prefix = format!("{}{}", prefix, n);
+        match v {
+            Value::String(x) => println!("{} = {}", prefix, x),
+            Value::Number(x) => println!("{} = {}", prefix, x),
+            Value::Bool(x) => println!("{} = {}", prefix, x),
+            Value::Object(_) => transform(v, &format!("{}__", &prefix)).unwrap(),
+            _ => println!("unknown"),
+        }
+    }
+    Ok(())
 }
 
 fn read_json_from_file<P: AsRef<Path>>(path: P) -> Result<Value, Box<dyn Error>> {
     let file = File::open(path)?;
-    let reader = BufReader::new(file);
+    let mut reader = BufReader::new(file);
+    let mut buf = vec![0; 3];
+    reader.read_exact(&mut buf)?;
+
+    if !is_bom(&buf) {
+        reader.seek(SeekFrom::Start(0))?;
+    }
+
     let u: Value = serde_json::from_reader(reader)?;
     Ok(u)
 }
 
-fn read_json_from_file2<P: AsRef<Path>>(path: P) -> Result<Value, Box<dyn Error>> {
-    let mut file = File::open(path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let u: Value = serde_json::from_str(&contents)?;
-    Ok(u)
+fn is_bom(buf: &[u8]) -> bool {
+    buf[0] == 0xef && buf[1] == 0xbb && buf[2] == 0xbf
 }
